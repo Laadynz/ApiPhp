48dd158d3d67e12d00f5a3678081a5f2d9c4c4ee
LaadyNz <laadynz@hotmail.com>
2024-06-22 19:36:55 -0700

    agregando api

--- productos.php
+++ productos.php
@@ -3,26 +3,26 @@ header("Content-Type: application/json");
 if(!empty($_GET['codigo']))
 {
   $codigo = $_GET['codigo'];
-  $mysqli = new mysqli("localhost", "root", "", "pos2");
-  $result = $mysqli->query("SELECT nombre_producto, precio FROM productos WHERE codigo = '$codigo'");
+  $mysqli = new mysqli("localhost", "root", "", "sistemapos");
+  $result = $mysqli->query("SELECT nombre, precio FROM productos WHERE codigo = '$codigo'");
   $row = $result->fetch_assoc();
   
   if($row)
   {
-    $data["nombre_producto"] = $row['nombre_producto'];
+    $data["nombre"] = $row['nombre'];
     $data["precio"] = $row['precio'];
     response(200,$data); #200 es el codigo de respuesta cuando se encuentra el producto
   }
   else
   {
-    $data["nombre_producto"] = "";
+    $data["nombre"] = "";
     $data["precio"] = "";
     response(300,$data);#300 es el codigo de respuesta cuando no se encuentra el producto
   }
 }
 else
 {
-  $data["nombre_producto"] = "";
+  $data["nombre"] = "";
     $data["precio"] = "";
     response(400,$data);#400 es el codigo de respuesta cuando se llama mal al API
 }
@@ -32,7 +32,7 @@ function response($status,$data)
 {
   header("HTTP/1.1 ".$status);
   $response['status'] = $status;
-  $response['nombre_producto'] = $data['nombre_producto'];
+  $response['nombre'] = $data['nombre'];
   $response['precio'] = $data['precio'];
   $json_response = json_encode($response);
   echo $json_response;
